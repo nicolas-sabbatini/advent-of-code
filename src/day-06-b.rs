fa06cc57fa908693dd2b3ea9a247f0d3419f4d66
Nico <nik.code.things@gmail.com>
2021-12-08 12:51:29 -0300

    Day 06

---
+++ src/day-06-b.rs
@@ -0,0 +1,27 @@
+use helpers::load_input;
+
+mod helpers;
+
+fn main() {
+    // Load input
+    let input = load_input();
+    let mut fish_days: [u64; 10] = [0; 10];
+    for num in input[0].split(",") {
+        let num = num.parse::<usize>().unwrap();
+        fish_days[num] += 1;
+    }
+    for _i in 0..256 {
+        for i in 0..10 {
+            let num_fish = fish_days[i];
+            fish_days[i] = 0;
+            if i == 0 {
+                fish_days[7] += num_fish;
+                fish_days[9] += num_fish;
+            } else {
+                fish_days[i - 1] += num_fish;
+            }
+        }
+    }
+
+    println!("{}", fish_days.iter().fold(0, |sum, x| sum + x));
+}
