12920ba7531500ccfbcc2fe8c8225cc5af89b43c
Nico <nik.code.things@gmail.com>
2021-12-11 00:42:27 -0300

    Day 10

---
+++ src/day-10-a.rs
@@ -0,0 +1,43 @@
+use core::panic;
+use helpers::load_input;
+
+mod helpers;
+
+fn main() {
+    // Load input
+    let input = load_input();
+    let mut res = 0;
+    for line in input.iter() {
+        let mut chunk_open: Vec<char> = Vec::new();
+        'line_chek: for chr in line.chars() {
+            match chr {
+                '(' | '[' | '{' | '<' => chunk_open.push(chr),
+                _ => {
+                    let prev = (chunk_open.len() - 1) as isize;
+                    let error = if prev >= 0 {
+                        check_char(chr, chunk_open[prev as usize])
+                    } else {
+                        check_char(chr, 'n')
+                    };
+                    if error > 0 {
+                        res += error;
+                        break 'line_chek;
+                    }
+                    chunk_open.remove(prev as usize);
+                }
+            }
+        }
+    }
+    println!("{}", res);
+}
+
+fn check_char(c: char, target: char) -> usize {
+    match (c, target) {
+        (')', '(') | (']', '[') | ('}', '{') | ('>', '<') => 0,
+        (')', _) => 3,
+        (']', _) => 57,
+        ('}', _) => 1197,
+        ('>', _) => 25137,
+        _ => panic!("Error!"),
+    }
+}
---
+++ src/day-10-b.rs
@@ -0,0 +1,57 @@
+use helpers::load_input;
+
+mod helpers;
+
+fn main() {
+    // Load input
+    let input = load_input();
+    let mut line_score: Vec<usize> = Vec::new();
+    for line in input.iter() {
+        let mut chunk_open: Vec<char> = Vec::new();
+        let mut has_error = false;
+        'line_chek: for chr in line.chars() {
+            match chr {
+                '(' | '[' | '{' | '<' => chunk_open.push(chr),
+                _ => {
+                    let prev = (chunk_open.len() - 1) as isize;
+                    has_error = if prev >= 0 {
+                        check_char(chr, chunk_open[prev as usize])
+                    } else {
+                        check_char(chr, 'n')
+                    };
+                    if has_error {
+                        break 'line_chek;
+                    }
+                    chunk_open.remove(prev as usize);
+                }
+            }
+        }
+        if !has_error {
+            line_score.push(complete_score(&chunk_open));
+        }
+    }
+    let middle = line_score.len() / 2;
+    line_score.sort();
+    println!("{}", line_score[middle]);
+}
+
+fn check_char(c: char, target: char) -> bool {
+    match (c, target) {
+        (')', '(') | (']', '[') | ('}', '{') | ('>', '<') => false,
+        _ => true,
+    }
+}
+
+fn complete_score(chunk_open: &Vec<char>) -> usize {
+    let mut res = 0;
+    for chr in chunk_open.iter().rev() {
+        match chr {
+            '(' => res = res * 5 + 1,
+            '[' => res = res * 5 + 2,
+            '{' => res = res * 5 + 3,
+            '<' => res = res * 5 + 4,
+            _ => panic!("Error!"),
+        }
+    }
+    res
+}
