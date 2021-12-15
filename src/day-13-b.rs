cdc86d14c7ae62cbdcdd9cdb0567f803a3781758
Nico <nik.code.things@gmail.com>
2021-12-14 21:40:49 -0300

    Day 13

---
+++ src/day-13-b.rs
@@ -0,0 +1,100 @@
+use helpers::load_input;
+use std::cmp::max;
+
+mod helpers;
+
+enum Axis {
+    X,
+    Y,
+}
+
+fn main() {
+    // Load input
+    let input = load_input();
+    let mut paper_builder: Vec<(usize, usize)> = Vec::new();
+    let mut folds: Vec<(Axis, usize)> = Vec::new();
+    let mut max_x = 0;
+    let mut max_y = 0;
+    // Parse input
+    for line in input.iter() {
+        // Parse folds
+        if line.len() > 11 {
+            let mut line = line.split(" ").last().unwrap().split("=");
+
+            let axis = line.next().unwrap();
+            let row = line.next().unwrap().parse::<usize>().unwrap();
+
+            match axis {
+                "x" | "X" => folds.push((Axis::X, row)),
+                "y" | "Y" => folds.push((Axis::Y, row)),
+                _ => panic!("Panic!"),
+            }
+        // Parse dots
+        } else if line.len() > 0 {
+            let mut line = line.split(",");
+            let x = line.next().unwrap().parse::<usize>().unwrap();
+            let y = line.next().unwrap().parse::<usize>().unwrap();
+            paper_builder.push((x, y));
+            max_x = max(max_x, x);
+            max_y = max(max_y, y);
+        }
+    }
+    // Build paper
+    let mut paper: Vec<Vec<bool>> = vec![vec![false; max_x + 1]; max_y + 1];
+    for cords in paper_builder.iter() {
+        paper[cords.1][cords.0] = true;
+    }
+
+    // Fold paper
+    for f in folds {
+        match f {
+            (Axis::X, col) => {
+                for x_offset in 0..(paper[0].len() - col) {
+                    for y in 0..paper.len() {
+                        if x_offset > col {
+                            break;
+                        }
+                        paper[y][col - x_offset] =
+                            paper[y][col - x_offset] || paper[y][col + x_offset];
+                        paper[y][col + x_offset] = false;
+                    }
+                }
+            }
+            (Axis::Y, row) => {
+                for y_offset in 0..(paper.len() - row) {
+                    for x in 0..paper[0].len() {
+                        if y_offset > row {
+                            break;
+                        }
+                        paper[row - y_offset][x] =
+                            paper[row - y_offset][x] || paper[row + y_offset][x];
+                        paper[row + y_offset][x] = false;
+                    }
+                }
+            }
+        }
+    }
+    print_paper(&paper);
+}
+
+fn print_paper(paper: &Vec<Vec<bool>>) {
+    for line in paper.iter() {
+        let mut stored_empty = 0;
+        let mut has_printed_line = false;
+        for dot in line.iter() {
+            if *dot {
+                for _i in 0..stored_empty {
+                    print!(" ");
+                }
+                print!("*");
+                stored_empty = 0;
+                has_printed_line = true;
+            } else {
+                stored_empty += 1;
+            }
+        }
+        if has_printed_line {
+            print!("\n");
+        }
+    }
+}
