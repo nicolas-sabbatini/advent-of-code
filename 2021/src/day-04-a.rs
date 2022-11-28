38d7f8d1c434ae525911846e2e1d9975ca86977f
Nicolas Sabbatini <nicolas.sabbatini.vrech@modo.com.ar>
2022-11-28 00:45:21 -0300

    Update 2021 day 4 clippy

--- 2021/src/day-04-a.rs
+++ 2021/src/day-04-a.rs
@@ -10,46 +10,43 @@ fn main() {
     let mut cards = Vec::new();
     let mut indexs: HashMap<usize, Vec<(usize, usize, usize)>> = HashMap::new();
     // Iterate over input
-    for line_index in 1..input.len() {
-        match input[line_index].as_str() {
-            // If is an empty line add a card
-            "" => cards.push(Vec::new()),
-            // If is a list of numbers
-            _ => {
-                // Get the current card
-                let card_index = cards.len() - 1;
-                // Add a row
-                cards[card_index].push(Vec::new());
-                // Get row index
-                let row_index = cards[card_index].len() - 1;
-                // Fill the row
-                for num in input[line_index].split(" ") {
-                    // If there are not a number
-                    if num == "" {
-                        continue;
-                    }
-                    // Parse number
-                    let num = num.parse::<usize>().unwrap();
-                    // Add "num" to the card
-                    cards[card_index][row_index].push((false, num));
-                    // Get position on row
-                    let num_index = cards[card_index][row_index].len() - 1;
-                    // Save num position on the Hash Map
-                    let pos = (card_index, row_index, num_index);
-                    match indexs.get_mut(&num) {
-                        Some(saved_pos) => saved_pos.push(pos),
-                        None => {
-                            indexs.insert(num, vec![pos]);
-                        }
+    for line in &input[1..] {
+        if let "" = line.as_str() {
+            cards.push(Vec::new())
+        } else {
+            // Get the current card
+            let card_index = cards.len() - 1;
+            // Add a row
+            cards[card_index].push(Vec::new());
+            // Get row index
+            let row_index = cards[card_index].len() - 1;
+            // Fill the row
+            for num in line.split(' ') {
+                // If there are not a number
+                if num.is_empty() {
+                    continue;
+                }
+                // Parse number
+                let num = num.parse::<usize>().unwrap();
+                // Add "num" to the card
+                cards[card_index][row_index].push((false, num));
+                // Get position on row
+                let num_index = cards[card_index][row_index].len() - 1;
+                // Save num position on the Hash Map
+                let pos = (card_index, row_index, num_index);
+                match indexs.get_mut(&num) {
+                    Some(saved_pos) => saved_pos.push(pos),
+                    None => {
+                        indexs.insert(num, vec![pos]);
                     }
                 }
             }
         }
     }
     // Get the "random" numbers
-    'rand_nums: for num in input[0].split(",") {
+    'rand_nums: for num in input[0].split(',') {
         // If there are not a number
-        if num == "" {
+        if num.is_empty() {
             continue;
         }
         // Parse number
@@ -71,7 +68,7 @@ fn main() {
     }
 }
 
-fn check_card(card: &Vec<Vec<(bool, usize)>>) -> bool {
+fn check_card(card: &[Vec<(bool, usize)>]) -> bool {
     // Get size
     let rows = card.len();
     let cols = card[0].len();
@@ -96,8 +93,8 @@ fn check_card(card: &Vec<Vec<(bool, usize)>>) -> bool {
     // Check rows
     for col in 0..cols {
         let mut sum = 0;
-        for row in 0..rows {
-            if card[row][col].0 {
+        for row in card {
+            if row[col].0 {
                 sum += 1;
             } else {
                 break;
@@ -111,9 +108,9 @@ fn check_card(card: &Vec<Vec<(bool, usize)>>) -> bool {
     false
 }
 
-fn calculate_card_point(card: &Vec<Vec<(bool, usize)>>, last_num: usize) -> usize {
+fn calculate_card_point(card: &[Vec<(bool, usize)>], last_num: usize) -> usize {
     let mut points = 0;
-    for row in card.iter() {
+    for row in card {
         points += row.iter().fold(
             0,
             |prev, (mark, value)| if !mark { *value + prev } else { prev },
