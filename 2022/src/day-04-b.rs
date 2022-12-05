4b69a86c0627cb66720c62039d921e31c0e134b7
Nicolas Sabbatini <nicolas.sabbatini.vrech@modo.com.ar>
2022-12-05 10:44:01 -0300

    2022 day 04

---
+++ 2022/src/day-04-b.rs
@@ -0,0 +1,31 @@
+use helpers::load_input;
+
+mod helpers;
+
+fn overlap(first_range: Vec<usize>, second_range: Vec<usize>) -> bool {
+    if first_range[0] <= second_range[1] && first_range[1] >= second_range[0] {
+        return true;
+    }
+    false
+}
+
+fn main() {
+    // Load input
+    let input = load_input();
+    let mut overlap_ranges = 0;
+    for line in input {
+        let ranges = line.split(',').collect::<Vec<&str>>();
+        let first_range = ranges[0]
+            .split('-')
+            .map(|ch| ch.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        let second_range = ranges[1]
+            .split('-')
+            .map(|ch| ch.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        if overlap(first_range, second_range) {
+            overlap_ranges += 1
+        }
+    }
+    println!("{overlap_ranges}");
+}
