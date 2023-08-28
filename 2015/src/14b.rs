8b0155a94d151cb694efc1d94720bd09bb1f3b1c
Nicolas Sabbatini <nik.code.things@gmail.com>
2023-08-28 00:00:20 -0300

    day 14 2015

---
+++ 2015/src/14b.rs
@@ -0,0 +1,54 @@
+use input_loader::load_input;
+
+#[derive(Debug)]
+struct Reindeer {
+    flay_distance: usize,
+    flay_time: usize,
+    total_time: usize,
+    total_distance: usize,
+    elapsed_time: usize,
+    points: usize,
+}
+
+impl Reindeer {
+    fn step(&mut self) {
+        if (self.elapsed_time % self.total_time) < self.flay_time {
+            self.total_distance += self.flay_distance;
+        }
+        self.elapsed_time += 1;
+    }
+}
+
+fn main() {
+    // Load input
+    let input = load_input();
+    let mut olympics: Vec<Reindeer> = Vec::new();
+    for line in input {
+        let line = line.split_whitespace().collect::<Vec<&str>>();
+        let flay_distance = line[3].parse::<usize>().unwrap();
+        let flay_time = line[6].parse::<usize>().unwrap();
+        let rest_time = line[13].parse::<usize>().unwrap();
+        olympics.push(Reindeer {
+            flay_distance,
+            flay_time,
+            total_time: flay_time + rest_time,
+            total_distance: 0,
+            elapsed_time: 0,
+            points: 0,
+        });
+    }
+    for _ in 0..2503 {
+        for reindeer in olympics.iter_mut() {
+            reindeer.step();
+        }
+        olympics.sort_by_key(|r| r.total_distance);
+        let max_dist = olympics.last_mut().unwrap().total_distance;
+        for reindeer in olympics.iter_mut() {
+            if reindeer.total_distance == max_dist {
+                reindeer.points += 1;
+            }
+        }
+    }
+    let res = olympics.iter().max_by_key(|r| r.points).unwrap().points;
+    println!("{res}");
+}
