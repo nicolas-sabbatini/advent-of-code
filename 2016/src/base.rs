2c6c9b58797310115048b870e0ee90c846bb0d03
Nicolas Sabbatini <nik.code.things@gmail.com>
2024-01-03 23:55:02 -0300

    2016 day 01

---
+++ 2016/src/base.rs
@@ -0,0 +1,7 @@
+use input_loader::load_input;
+
+fn main() {
+    // Load input
+    let input = load_input();
+    println!("{input:?}");
+}
---
+++ 2016/src/day-01-b.rs
@@ -0,0 +1,67 @@
+use input_loader::load_input;
+
+enum LookingAt {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl LookingAt {
+    fn rotate_left(&mut self) {
+        *self = match self {
+            LookingAt::Up => LookingAt::Left,
+            LookingAt::Left => LookingAt::Down,
+            LookingAt::Down => LookingAt::Right,
+            LookingAt::Right => LookingAt::Up,
+        };
+    }
+
+    fn rotate_right(&mut self) {
+        *self = match self {
+            LookingAt::Up => LookingAt::Right,
+            LookingAt::Right => LookingAt::Down,
+            LookingAt::Down => LookingAt::Left,
+            LookingAt::Left => LookingAt::Up,
+        };
+    }
+
+    fn move_forward(&self, x: i32, y: i32) -> (i32, i32) {
+        match self {
+            LookingAt::Up => (x, y + 1),
+            LookingAt::Down => (x, y - 1),
+            LookingAt::Left => (x - 1, y),
+            LookingAt::Right => (x + 1, y),
+        }
+    }
+}
+
+fn main() {
+    let input = load_input();
+    let mut start_position = (0, 0);
+    let mut already_visited = vec![(0, 0)];
+    let mut looking_at = LookingAt::Up;
+    for command in input[0].split(", ") {
+        let command = command.chars().collect::<Vec<char>>();
+        let dir = command[0];
+        let steps = command[1..]
+            .iter()
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        match dir {
+            'L' => looking_at.rotate_left(),
+            'R' => looking_at.rotate_right(),
+            _ => panic!("Unknown direction"),
+        }
+        for _ in 0..steps {
+            start_position = looking_at.move_forward(start_position.0, start_position.1);
+            if already_visited.contains(&start_position) {
+                println!("{}", start_position.0.abs() + start_position.1.abs());
+                return;
+            }
+            already_visited.push(start_position);
+        }
+    }
+    println!("{}", start_position.0.abs() + start_position.1.abs());
+}
